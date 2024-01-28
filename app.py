38f74f7e9f57e74fa4a2ae3e2daf6b16dd8f3be4
Abhay Mhatre <abhaymhatre00@gmail.com>
2024-01-28 20:29:31 +0530

    app.py changes added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np 
-import pickle as pk 
-import streamlit as st 
+import numpy as np
+import pickle as pk
+import streamlit as st
 
-model = pk.load(open('model.pkl', 'wb'))
+model = pk.load(open('model.pkl', 'rb'))
 
 st.header('Car Price Prediction ML Model')
